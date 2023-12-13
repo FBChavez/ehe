f64bb3d8a34a0beb41f8b6369949884acedd9da6
PAUL ABELLANA <paulmabellana@gmail.com>
2023-12-14 01:27:36 +0800

    from pol

---
+++ AllRecipes/LecheFlaniFrame.java
@@ -0,0 +1,45 @@
+package AllRecipes;
+
+import MainPackage.RecipeFrame;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LecheFlaniFrame extends JFrame {
+
+    private String playerName;
+    private JPanel jPanel;
+    private JTextPane descriptionTP;
+    private JScrollPane descriptionSP;
+    private JList ingredientsList;
+    private JScrollPane ingredientsSP;
+    private JList instructionsList;
+    private JScrollPane instructionsSP;
+    private JScrollPane notesSP;
+    private JList notesList;
+    private JButton backToRecipePageButton;
+    private JLabel recipenameLabel;
+    private JLabel foodImg;
+
+    public LecheFlaniFrame(String name) {
+        this.playerName = name;
+
+
+        setTitle("Christmas Recipes");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 675);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setContentPane(jPanel);
+        backToRecipePageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                RecipeFrame recipeFrame = new RecipeFrame(name);
+                recipeFrame.setVisible(true);
+            }
+        });
+    }
+
+}
